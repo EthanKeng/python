007d6db07d186e758fc5490df8533790a53351da
EthanKeng <36765774+EthanKeng@users.noreply.github.com>
2021-04-27 15:31:05 +0900

    Create find_u200b_invisible_space.py

---
+++ find_u200b_invisible_space.py
@@ -0,0 +1,14 @@
+import csv
+
+with open('feed.csv', encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        for c in row:
+            if c.find(u'\u200b')>-1 :
+                print("見えないゴミは第",line_count,"行目")
+                print(c)
+                print("場所は第",c.find(u'\u200b'),"の文字に")
+                print("------")
+    line_count += 1
+    print(f'Processed {line_count} lines.')
