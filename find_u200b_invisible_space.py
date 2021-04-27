72082ae79322ae70d7dac8b2d47ecda6291af165
EthanKeng <36765774+EthanKeng@users.noreply.github.com>
2021-04-27 15:46:30 +0900

    correct the line_count increment

--- find_u200b_invisible_space.py
+++ find_u200b_invisible_space.py
@@ -1,14 +1,31 @@
 import csv
 
-with open('feed.csv', encoding="utf8") as csv_file:
+with open('criteo_feed.csv', encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         for c in row:
             if c.find(u'\u200b')>-1 :
-                print("見えないゴミは第",line_count,"行目")
+                print("The invisible char is at",line_count+1,"th line")
                 print(c)
-                print("場所は第",c.find(u'\u200b'),"の文字に")
+                print("At the ",c.find(u'\u200b')+1,"th position of the word above")
                 print("------")
-    line_count += 1
+        line_count += 1
     print(f'Processed {line_count} lines.')
+    
+
+# ---Japanese version--
+# import csv
+
+# with open('criteo_feed.csv', encoding="utf8") as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         for c in row:
+#             if c.find(u'\u200b')>-1 :
+#                 print("見えないゴミは第",line_count+1,"行目")
+#                 print(c)
+#                 print("場所は第",c.find(u'\u200b')+1,"の文字に")
+#                 print("------")
+#         line_count += 1
+#     print(f'Processed {line_count} lines.')
