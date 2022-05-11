e1597e7d5eb67589301b256b4b3561f0af6fd75d
YK <36765774+EthanKeng@users.noreply.github.com>
2022-05-11 11:25:21 +0900

    Create task.py

---
+++ pillow/task.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+path_to_image = 'images/'
+image_files = [img for img in os.listdir(path_to_image) if img.s$
+# print(len(image_files))
+
+
+for i in image_files:
+  im = Image.open(os.path.join(path_to_image, i))
+
+  im = im.rotate(angle=90).resize((128, 128)).convert('RGB')
+#  im.show()
+  print(im.format, im.size, im.mode)
+
+  im.save(os.path.join("/opt/icons/",i),format="jpeg")
